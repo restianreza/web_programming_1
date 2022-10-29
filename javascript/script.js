c15387eca5ebc08913b2d22baefd073ae160bfb6
restianreza <114054889+restianreza@users.noreply.github.com>
2022-10-29 08:10:57 +0700

    update

--- javascript/script.js
+++ javascript/script.js
@@ -14,65 +14,44 @@
 // console.log(mahasiswa.nama) // output Reza
 // console.log(mahasiswa.hobi[0]) // output Ngoding
 
+
 let txtNpm = document.getElementById("npm")
 let txtNama = document.getElementById("nama")
-let listMhs = document.getElementById("listMahasiswa")
+let lishMhs = document.getElementById("listMahasiswa")
 let tblMhs = document.getElementById("tblMahasiswa")
 
 let data = []
-// panggil funtion tampil()
-tampil()
 
 function simpan() {
-    console.log("Button Simpan Ditekan")
-
+    console.log("Button simpan ditekan")
     console.log(txtNpm.value)
     console.log(txtNama.value)
-    //simpan 
 
-
-    //cek apakah ada data di local storagenya
-    if (localStorage.getItem("lsMahasiswa") === null) {
-        //jika localStorage dengan key lsMahasiswa belum ada
+    if(localStorage.getItem("lsMahasiswa")===null){
         data.push({
-            "npm": txtNpm.value,
-            "nama": txtNama.value
-        })
-        //simpan localStorage dengan Key lsMahasiswa 
+            "npm" : txtNpm.value,
+            "nama" : txtNama.value}
+        )
         localStorage.setItem("lsMahasiswa", JSON.stringify(data))
     } else {
-        //jika localStorage dengan key lsMahasiswa sudah ada/sudah disimpan
-
-        //Ambil dulu localStorage dengan key lsMahasiswa
         let dataLs = JSON.parse(localStorage.getItem("lsMahasiswa"))
         console.log(dataLs)
-        //push data baru ke dalam array
-        dataLs.push({
-            "npm": txtNpm.value,
-            "nama": txtNama.value
-        })
-        //simpan data baru ke dalam locakStorage
+        dataLs.push({"npm" : txtNpm.value, "nama" : txtNama.value})
         localStorage.setItem("lsMahasiswa", JSON.stringify(dataLs))
-
     }
 
+    tampil()
 }
 
 function tampil() {
-    // clear elemen listMahasiswa
-    listMhs.innerHTML = ""
-    //gunakan forEach
+    lishMhs.innerHTML = ""
     data.forEach(listData)
-
-    //ambil data local storage dengan key lsMahasiswa
     let dataTampil = JSON.parse(localStorage.getItem("lsMahasiswa"))
     dataTampil.forEach(listData)
-
 }
 
 function listData(item, index) {
-    // innerHtml elemen table id = "listMahasiswa" pada index.html
-    listMhs.innerHTML += "<li>" + item.npm + "-" + item.nama + "</li>"
-    // innerHtml elemen table id = "tblMahasiswa" pada index.html
+    lishMhs.innerHTML += "<li>" + item.npm + "-" + item.nama + "</li>"
+
     tblMhs.innerHTML += `<tr><td>${item.npm}</td><td>${item.nama}</td><tr>`
 }
